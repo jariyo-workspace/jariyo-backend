76220c1e17b4a2f4f740a8e645ddb7e813585a0a
N0067H <n006h3417h@gmail.com>
2026-07-15 00:18:06 +0900

    init

---
+++ src/main/java/com/example/jariyo_backend/JariyoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.jariyo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JariyoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JariyoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jariyo_backend/JariyoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jariyo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JariyoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
